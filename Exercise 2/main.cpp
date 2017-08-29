df8b7492a555c15055889b9128173df6710d05f4
Rafael Pintar Alevato <rpa.alevato@gmail.com>
2017-08-29 20:30:31 -0300

    Created timeOneSecond
    Created a function that generates a boolean when one seconds has passed.

--- Exercise 2/main.cpp
+++ Exercise 2/main.cpp
@@ -1,30 +1,55 @@
 #include "ClockCalendar.cpp"
 #include <iostream>
+#include <time.h>
 
 using namespace std;
+
+bool timeOneSecond (time_t* timeStart, time_t* timeNow, double* oldElapsed);
 
 int main()
 {
     bool secondAdvanced = false;
-    ClockCalendar cc (2017, 12, 30, 12, 59, 58, true);
+    ClockCalendar cc (2017, 8, 29, 07, 15, 30, true);
     unsigned int year, month, day, hour, minute, second;
-
     bool isPM;
+    double oldElapsed;
+    time_t timeStart, timeNow;
+
+    time(&timeStart);
+
     while (true) {
-        cin >> secondAdvanced;
+        secondAdvanced = timeOneSecond(&timeStart, &timeNow, &oldElapsed);
         if (secondAdvanced) {
             secondAdvanced = false;
             cc.advance();
             cc.readCalendar(year, month, day);
             cc.readClock(hour, minute, second, isPM);
-            cout << "Year: " << year << endl;
-            cout << "Month: " << month << endl;
-            cout << "Day: " << day << endl;
-            cout << "Hour: " << hour << endl;
-            cout << "Minute: " << minute << endl;
-            cout << "Second: " << second << endl;
-            cout << "isPM: " << isPM << endl;
+            cout << year << "/";
+            cout << month << "/";
+            cout << day << " - ";
+            cout << hour << ":";
+            cout << minute << ":";
+            cout << second;
+            if (isPM)
+                cout << " pm";
+            else
+                cout << " am";
+            cout << endl << endl;
         }
     }
     return 0;
+}
+
+bool timeOneSecond (time_t* timeStart, time_t* timeNow, double* oldElapsed) {
+    bool secondAdvanced = false;
+    double elapsed;
+    while (!secondAdvanced) {
+        time(timeNow);
+        elapsed = difftime(*timeNow, *timeStart);
+        if (elapsed > *oldElapsed) {
+            secondAdvanced = true;
+        }
+    }
+    *oldElapsed = elapsed;
+    return secondAdvanced;
 }
