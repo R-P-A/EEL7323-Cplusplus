80558cf8bc8c9c43199f130d77d86d85f9e26454
Rafael Pintar Alevato <rpa.alevato@gmail.com>
2017-10-19 13:05:57 -0200

    Working oled tests with all functions. Next step is to make it a class.

--- Exercise 4 - VendingMachine/tests/oledTest.cpp
+++ Exercise 4 - VendingMachine/tests/oledTest.cpp
@@ -10,7 +10,16 @@ int main(void) {
 	oledClear();
 	setLine(1);
 	printString(helloWorld);
-	delay(500000);
+	helloWorld = "Hi 123";
+	delay(5000000);
+	clearLine(1);
+	setLine(2);
+	printString(helloWorld);
+	delay(5000000);
+	oledClear();
+	delay(5000000);
+	oledFill();
+	delay(5000000);
 	oledOff();
 	return 0;
 }
